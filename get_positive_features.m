fcb1ed9c8a4a74c5ce029efe1d5b34ece3b3c7b9
Patrick Moore <phatrickmoore@gmail.com>
2017-04-25 19:40:02 -0500

    first stab at getting positives

--- get_positive_features.m
+++ get_positive_features.m
@@ -31,5 +31,13 @@ function features_pos = get_positive_features(train_path_pos, feature_params)
 image_files = dir( fullfile( train_path_pos, '*.jpg') ); %Caltech Faces stored as .jpg
 num_images = length(image_files);
 
-% placeholder to be deleted
-features_pos = rand(100, (feature_params.template_size / feature_params.hog_cell_size)^2 * 31);
+features_pos = zeros(num_images,...
+    (feature_params.template_size / feature_params.hog_cell_size)^2 * 31);
+
+for i = 1:num_images
+    img = im2single(imread([image_files(i).folder '/' image_files(i).name]));
+    hog = vl_hog(img, feature_params.hog_cell_size);
+    features_pos(i,:) = reshape(hog, 1, []);
+end
+
+
